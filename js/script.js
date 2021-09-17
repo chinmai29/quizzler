5b1ebdc069c221a3584a7b057dd50cf71ad3107d
chinmai29 <chinmaigoyal7@gmail.com>
2021-09-17 23:09:30 +0530

    score code updated

--- js/script.js
+++ js/script.js
@@ -121,13 +121,14 @@ function optionSelected(answer) {
     const allOptions = option_list.children.length; //getting all option items
 
     if (userAns == correcAns) { //if user selected option is equal to array's correct answer
-        userScore += 1; //upgrading score value with 1
+        userScore += 4; //add 4 if the answer is correct
         answer.classList.add("correct"); //adding green color to correct selected option
         answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
         // console.log("Correct Answer");
         // console.log("Your correct answers = " + userScore);
     } else {
         answer.classList.add("incorrect"); //adding red color to correct selected option
+        userScore -= 1; // subtract 1 if the answer is incorrect.
         answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
         // console.log("Wrong Answer");
 
@@ -152,13 +153,13 @@ function showResult() {
     const scoreText = result_box.querySelector(".score_text");
     if (userScore > 3) { // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! , You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        let scoreTag = '<span>and congrats! , You got <p>' + userScore + '</p> out of <p>' + questions.length * 4 + '</p></span>';
         scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
     } else if (userScore > 1) { // if user scored more than 1
-        let scoreTag = '<span>and nice , You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        let scoreTag = '<span>and nice , You got <p>' + userScore + '</p> out of <p>' + questions.length * 4 + '</p></span>';
         scoreText.innerHTML = scoreTag;
     } else { // if user scored less than 1
-        let scoreTag = '<span>and sorry , You got only <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        let scoreTag = '<span>and sorry , You got only <p>' + userScore + '</p> out of <p>' + questions.length * 4 + '</p></span>';
         scoreText.innerHTML = scoreTag;
     }
 }
