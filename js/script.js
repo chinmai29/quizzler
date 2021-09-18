a963e82e4afd1c6a41bb60c1bfef716e6af32aa1
chinmai29 <chinmaigoyal7@gmail.com>
2021-09-18 23:42:54 +0530

    warning code completed

--- js/script.js
+++ js/script.js
@@ -151,11 +151,11 @@ function showResult() {
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3) { // if user scored more than 3
+    if (userScore >= 15) { // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>and congrats! , You got <p>' + userScore + '</p> out of <p>' + questions.length * 4 + '</p></span>';
         scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
-    } else if (userScore > 1) { // if user scored more than 1
+    } else if (userScore >= 10) { // if user scored more than 1
         let scoreTag = '<span>and nice , You got <p>' + userScore + '</p> out of <p>' + questions.length * 4 + '</p></span>';
         scoreText.innerHTML = scoreTag;
     } else { // if user scored less than 1
